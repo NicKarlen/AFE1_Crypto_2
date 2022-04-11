22f6ee1bb62b74a6e08651b740e5ee76801659f2
NicKarlen <nicolas.k@gmx.net>
2022-04-11 15:53:01 +0200

    func.py

--- func.py
+++ func.py
@@ -52,16 +52,18 @@ def get_1min_Bitpanda(tradingpair):
     t_15min_ago = t_now - timedelta(minutes=15)
 
     dict_t_now = {
-        "year": str(t_now.year),
-        "month": str(t_now.month) if len(str(t_now.month)) == 2 else "0" + str(t_now.month),
-        "day": str(t_now.day) if len(str(t_now.day)) == 2 else "0" + str(t_now.day),
+        # "year": str(t_now.year),
+        # "month": str(t_now.month) if len(str(t_now.month)) == 2 else "0" + str(t_now.month),
+        # "day": str(t_now.day) if len(str(t_now.day)) == 2 else "0" + str(t_now.day),
+        "date":  str(t_now.date()),
         "hour": str(t_now.hour) if len(str(t_now.hour)) == 2 else "0" + str(t_now.hour),
         "minute": str(t_now.minute) if len(str(t_now.minute)) == 2 else "0" + str(t_now.minute)
     }
     dict_t_15min_ago = {
-        "year": str(t_15min_ago.year),
-        "month": str(t_15min_ago.month) if len(str(t_15min_ago.month)) == 2 else "0" + str(t_15min_ago.month),
-        "day": str(t_15min_ago.day) if len(str(t_15min_ago.day)) == 2 else "0" + str(t_15min_ago.day),
+        # "year": str(t_15min_ago.year),
+        # "month": str(t_15min_ago.month) if len(str(t_15min_ago.month)) == 2 else "0" + str(t_15min_ago.month),
+        # "day": str(t_15min_ago.day) if len(str(t_15min_ago.day)) == 2 else "0" + str(t_15min_ago.day),
+        "date":  str(t_15min_ago.date()),
         "hour": str(t_15min_ago.hour) if len(str(t_15min_ago.hour)) == 2 else "0" + str(t_15min_ago.hour),
         "minute": str(t_15min_ago.minute) if len(str(t_15min_ago.minute)) == 2 else "0" + str(t_15min_ago.minute)
     }
@@ -69,7 +71,7 @@ def get_1min_Bitpanda(tradingpair):
     print(dict_t_now)
     print(dict_t_15min_ago)
 
-    url = f"https://api.exchange.bitpanda.com/public/v1/candlesticks/{tradingpair}?unit=MINUTES&period=1&from=2022-04-11T04%3A59%3A59.999Z&to=2022-04-11T07%3A59%3A59.999Z"
+    #url = f"https://api.exchange.bitpanda.com/public/v1/candlesticks/{tradingpair}?unit=MINUTES&period=1&from={dict_t_15min_ago["year"]}-{dict_t_15min_ago[]}-11T04%3A59%3A59.999Z&to=2022-04-11T07%3A59%3A59.999Z"
     
     
     # req = requests.get(url)
