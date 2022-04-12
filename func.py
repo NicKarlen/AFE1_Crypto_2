7506f9ed028b0991e9b01eb2a76cba73a1d7bf80
NicKarlen <nicolas.k@gmx.net>
2022-04-12 10:07:50 +0200

    New exchange Bitstamp

--- func.py
+++ func.py
@@ -17,7 +17,8 @@ def get_1min_Upbit(tradingpair):
 
     return {
         "tradingpair": resp['result'][0]['market'],
-        "timestamp": resp['result'][0]['candle_date_time_utc'][:10] + ' ' + resp['result'][0]['candle_date_time_utc'][11:],
+        #"timestamp": resp['result'][0]['candle_date_time_utc'][:10] + ' ' + resp['result'][0]['candle_date_time_utc'][11:],
+        "timestamp": str(datetime.fromtimestamp(int(str(resp["result"][0]["timestamp"])[:10]))),
         "price_low": resp['result'][0]['low_price'],
         "price_high": resp['result'][0]['high_price'],
         "price_close": resp['result'][0]['trade_price'],
@@ -44,7 +45,29 @@ def get_1min_Coinbase(tradingpair):
     }
 
 """
+    Bitstamp: get 1 minute candle from the given tradingpair
+"""
+
+def get_1min_Bitstamp(tradingpair):
+
+    url = f"https://www.bitstamp.net/api/v2/ohlc/{tradingpair}/?step=60&limit=1"
+    req = requests.get(url)
+    json_response = json.loads(req.text)
+
+    return {
+        "tradingpair": json_response["data"]["pair"][:3] + "-" + json_response["data"]["pair"][4:],
+        "timestamp": str(datetime.fromtimestamp(int(json_response["data"]["ohlc"][0]["timestamp"]))),
+        "price_low": json_response["data"]["ohlc"][0]["low"],
+        "price_high": json_response["data"]["ohlc"][0]["high"],
+        "price_close": json_response["data"]["ohlc"][0]["close"],
+        "volume": json_response["data"]["ohlc"][0]["volume"]
+    }
+
+
+"""
     Bitpanda: get the 1 minute candle for the tradingpair
+
+    WAS NOT USED. VOLUME ON EXCHANGE IS TO LOW: 
 """
 def get_1min_Bitpanda(tradingpair):
 
@@ -71,20 +94,15 @@ def get_1min_Bitpanda(tradingpair):
     print(dict_t_now)
     print(dict_t_15min_ago)
 
-    #url = f"https://api.exchange.bitpanda.com/public/v1/candlesticks/{tradingpair}?unit=MINUTES&period=1&from={dict_t_15min_ago["year"]}-{dict_t_15min_ago[]}-11T04%3A59%3A59.999Z&to=2022-04-11T07%3A59%3A59.999Z"
+    url = f'https://api.exchange.bitpanda.com/public/v1/candlesticks/{tradingpair}?unit=MINUTES&period=1&from={dict_t_15min_ago["date"]}T{dict_t_15min_ago["hour"]}%3A{dict_t_15min_ago["minute"]}%3A59.999Z&to={dict_t_now["date"]}T{dict_t_now["hour"]}%3A{dict_t_now["minute"]}%3A59.999Z'
     
+    print(url)
     
-    # req = requests.get(url)
-    # json_response = json.loads(req.text)
+    req = requests.get(url)
+    json_response = json.loads(req.text)
 
-    # return {
-    #     "tradingpair": tradingpair,
-    #     "timestamp": str(datetime.fromtimestamp(json_response[0][0])),
-    #     "price_low": json_response[0][1],
-    #     "price_high": json_response[0][2],
-    #     "price_close": json_response[0][4],
-    #     "volume": json_response[0][5]
-    # }
+    print(json_response)
+
 
 
 # def calc_VWAP(dict_1min_candle):
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import func
 
-# dict0 = func.get_1min_Upbit("KRW-BTC")
+dict0 = func.get_1min_Upbit("KRW-BTC")
 
-# print(dict0)
+print(dict0)
 
-# dict1 = func.get_1min_Coinbase("BTC-USD")
+dict1 = func.get_1min_Coinbase("BTC-USD")
 
-# print(dict1)
+print(dict1)
 
-func.get_1min_Bitpanda("BTC_EUR")
+dict2 = func.get_1min_Bitstamp("btceur")
+
+print(dict2)
