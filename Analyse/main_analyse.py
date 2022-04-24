ff68b8c4da16a06a1f09055c10db4e789f80959a
NicKarlen <nicolas.k@gmx.net>
2022-04-24 20:19:20 +0200

    Update main_analyse.py

--- Analyse/main_analyse.py
+++ Analyse/main_analyse.py
@@ -12,26 +12,26 @@ con = sqlite3.connect(f"Analyse/DB/{db_name}")
 
 # read the table of the arbitrage index
 df_arb = pd.read_sql_query("SELECT timestamp, arbitrage_index FROM Arbitrage_Index", con)
-df_vol_Upbit = pd.read_sql_query("SELECT timestamp, volume FROM One_Min_Candles WHERE exchange LIKE 'Upbit' ORDER BY timestamp", con)
-df_vol_Coinbase = pd.read_sql_query("SELECT timestamp, volume FROM One_Min_Candles WHERE exchange LIKE 'Coinbase' ORDER BY timestamp", con)
-df_vol_Bitstamp = pd.read_sql_query("SELECT timestamp, volume FROM One_Min_Candles WHERE exchange LIKE 'Bitstamp' ORDER BY timestamp", con)
+df_Upbit = pd.read_sql_query("SELECT timestamp, volume, avg_price_USD FROM One_Min_Candles WHERE exchange LIKE 'Upbit' ORDER BY timestamp", con)
+df_Coinbase = pd.read_sql_query("SELECT timestamp, volume, avg_price_USD FROM One_Min_Candles WHERE exchange LIKE 'Coinbase' ORDER BY timestamp", con)
+df_Bitstamp = pd.read_sql_query("SELECT timestamp, volume, avg_price_USD FROM One_Min_Candles WHERE exchange LIKE 'Bitstamp' ORDER BY timestamp", con)
 
 # Close our connection
 con.close()
 
 # merge the volume of the tree exchanges to the df
-df = pd.merge(df_arb,df_vol_Upbit, on=['timestamp'])
-df.rename(columns={'volume': 'vol_Upbit'}, inplace=True)
+df = pd.merge(df_arb,df_Upbit, on=['timestamp'])
+df.rename(columns={'volume': 'vol_Upbit', "avg_price_USD": "avg_price_USD_Upbit"}, inplace=True)
 
-df = pd.merge(df,df_vol_Coinbase, on=['timestamp'])
-df.rename(columns={'volume': 'vol_Coinbase'}, inplace=True)
+df = pd.merge(df,df_Coinbase, on=['timestamp'])
+df.rename(columns={'volume': 'vol_Coinbase', "avg_price_USD": "avg_price_USD_Coinbase"}, inplace=True)
 
-df = pd.merge(df,df_vol_Bitstamp, on=['timestamp'])
-df.rename(columns={'volume': 'vol_Bitstamp'}, inplace=True)
+df = pd.merge(df,df_Bitstamp, on=['timestamp'])
+df.rename(columns={'volume': 'vol_Bitstamp', "avg_price_USD": "avg_price_USD_Bitstamp"}, inplace=True)
 
 
 # Create a plot-window with tree seperate plots with a shared X axis
-fig, axes = plt.subplots(nrows=3, sharex=True)
+fig, axes = plt.subplots(nrows=4, sharex=True)
 
 # Output a plot of the arbitrage index with the volume of Upbit behind
 df.plot(kind="line",x="timestamp", y=["vol_Upbit", "arbitrage_index"],
@@ -47,6 +47,10 @@ df.plot(kind="line",x="timestamp", y=["vol_Coinbase", "arbitrage_index"],
 # Output a plot of the arbitrage index with the volume of Bitstamp behind
 df.plot(kind="line",x="timestamp", y=["vol_Bitstamp", "arbitrage_index"],
         color=["lightblue", "red"], secondary_y=["arbitrage_index"], ax=axes[2])
+
+# Output a plot of the avg prices on exchanges
+df.plot(kind="line",x="timestamp", y=["avg_price_USD_Upbit", "avg_price_USD_Coinbase", "avg_price_USD_Bitstamp"],
+        color=["blue", "black", "green"], secondary_y=["arbitrage_index"], ax=axes[3])
 plt.show()
 
 
